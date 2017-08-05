1bb5120c1709eda2417e7a06bc5cea0d43209893
work1 <abc@abcmail.com>
2017-08-06 00:41:28 +0530

    Initial Commit.

---
+++ SalesData/src/ca/demo/salestool/Main.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package ca.demo.salestool;
+
+/**
+ * @author Admin
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SalesData data= new SalesData();
+		displayGreeting();
+		data.display();
+	}
+
+	private static void displayGreeting() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Sales people");
+		System.out.println("The app shows sales data");
+	}
+
+}
---
+++ SalesData/src/ca/demo/salestool/SalesData.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package ca.demo.salestool;
+
+/**
+ * @author Admin
+ *
+ */
+public class SalesData {
+
+	public void display() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("TO DO: Data display");		
+	}
+
+}
