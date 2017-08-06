01269a36885c58643b86d45082d8cda202ce2ae0
work1 <abc@abcmail.com>
2017-08-06 18:31:38 +0530

    adding iterator.

--- SalesData/src/ca/demo/salestool/SalesData.java
+++ SalesData/src/ca/demo/salestool/SalesData.java
@@ -8,11 +8,18 @@ package ca.demo.salestool;
  *
  */
 public class SalesData {
+	
+	int[] x= {1,2,3};
 
 	public void display() {
 		// TODO Auto-generated method stub
 		
-		System.out.println("TO DO: Data display");		
+		System.out.println("TO DO: Data display");	
+		
+		for(int i=0; i<x.length; i++)
+		{
+			System.out.println(x[i]);
+		}
 	}
 
 }
